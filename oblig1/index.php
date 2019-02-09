562c7bb758aefa27a167a1c9d0c80c9b29fb5268
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-02-09 15:26:51 +0100

    Check if a menu item is chosen

--- oblig1/index.php
+++ oblig1/index.php
@@ -1,6 +1,12 @@
 <?php require("head.php"); ?>
 <?php require("header.php"); ?>
 <main>
-	<h2>test</h2>
+	<?php 
+		if($chosenMovie !== null) {		
+			@include 'movie-template.php';
+		} else {
+			echo "<p class='choose-movie'>Select a movie from the menu</p>";
+		}
+	?>
 </main>
 <?php require("footer.php"); ?>
