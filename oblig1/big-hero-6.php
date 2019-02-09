6001efffe17d84cfc2869b9fbadae176a6969382
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-02-09 15:21:55 +0100

    Added two more movies

---
+++ oblig1/big-hero-6.php
@@ -0,0 +1,8 @@
+<?php 
+		$title	=	'Big Hero 6';
+		$img	=	'./img/bighero6.png';
+		$desc	=	'The special bond that develops between plus-sized inflatable robot Baymax, and prodigy Hiro Hamada, who team up with a group of friends to form a band of high-tech heroes.';
+		$lead	=	'Ryan Potter';
+		$link	=	'https://www.imdb.com/title/tt2245084/';
+?>
+
---
+++ oblig1/coco.php
@@ -0,0 +1,8 @@
+<?php 
+		$title	=	'Coco';
+		$img	=	'./img/coco.png';
+		$desc	=	'Aspiring musician Miguel, confronted with his familys ancestral ban on music, enters the Land of the Dead to find his great-great-grandfather, a legendary singer.';
+		$lead	=	'Anthony Gonzalez';
+		$link	=	'https://www.imdb.com/title/tt2380307/';
+?>
+
