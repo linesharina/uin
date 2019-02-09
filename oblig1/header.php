1fab79619f9e7be5da00d126f639a6d29a98b407
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-02-09 15:25:59 +0100

    Add logic for finding out which menu item are chosen

--- oblig1/header.php
+++ oblig1/header.php
@@ -1,7 +1,16 @@
 <header>
-	<h1>Filmarkiv</h1>
+	<h1><a href="index.php">Movie archive</a></h1>
 	<nav>
-		<a href="index.php">Forside</a>
-		<a href="tangled.php">Tangled</a>
+		<?php 
+			foreach ($movies as $movie_slug => $movie_name) {
+				$nav_item_class = 'nav-item';
+
+				if ($chosenMovie == $movie_slug) {
+					$nav_item_class .= ' nav-item-active';
+				}
+
+				echo '<a href="./?movie=' . $movie_slug . '" class="' . $nav_item_class . '">' . $movie_name . '</a>';
+			}
+		?>
 	</nav>
 </header>
