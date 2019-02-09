82878e4d3fb067560bd1d1a88e48848e050ffab2
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-02-09 15:25:14 +0100

    Check which movie are chosen

--- oblig1/head.php
+++ oblig1/head.php
@@ -1,3 +1,29 @@
+<?php
+	
+	$chosenMovie = null;
+
+	if(isset($_GET['movie'])) {
+		$chosenMovie = $_GET['movie'];
+
+		if($chosenMovie == 'tangled') {
+			@include 'tangled.php';
+		}
+
+		if($chosenMovie == 'bighero6') {
+			@include 'big-hero-6.php';
+		}
+
+		if($chosenMovie == 'coco') {
+			@include 'coco.php';
+		}
+	}
+
+	$movies = [
+		'tangled' => 'Tangled',
+		'bighero6' => 'Big Hero 6',
+		'coco' => 'Coco'
+	];
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,4 +32,4 @@
 	<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 </head>
-<body>
+<body class="<?php echo $chosenMovie; ?>">
