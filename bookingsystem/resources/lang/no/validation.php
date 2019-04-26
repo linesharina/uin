82d3941ee92340879d6ab42e9cbeb56fe4505bb1
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-26 22:13:56 +0200

    Add flash messages and translation

---
+++ bookingsystem/resources/lang/no/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'required' => ':attribute er påkrevd.',
+
+    'attributes' => [
+        'when_checkin-day' => 'Dag for innsjekk',
+        'when_checkin-month' => 'Måned for innsjekk',
+        'when_checkin-year' => 'År for innsjekk',
+        'when_checkout-day' => 'Dag for utsjekk',
+        'when_checkout-month' => 'Måned for utsjekk',
+        'when_checkout-year' => 'År for utsjekk',
+        'when_number'  => 'Antall personer'
+    ],
+];
--- bookingsystem/resources/views/layouts/app.blade.php
+++ bookingsystem/resources/views/layouts/app.blade.php
@@ -73,6 +73,7 @@
         </nav>
 
         <main class="py-4">
+            @include('layouts.flash-messages')
             @yield('content')
         </main>
     </div>
