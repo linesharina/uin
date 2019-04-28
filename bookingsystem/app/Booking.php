6031c8754bee44ec81367c554bb257e9c736107c
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-28 22:32:25 +0200

    Add db relations

--- bookingsystem/app/Booking.php
+++ bookingsystem/app/Booking.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    public function booking_rooms()
+    {
+        return $this->hasMany('App\BookingRoom');
+    }
 }
--- bookingsystem/app/BookingRoom.php
+++ bookingsystem/app/BookingRoom.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class BookingRoom extends Model
 {
-    //
+    public function booking()
+    {
+        return $this->belongsTo('App\Booking');
+    }
 }
