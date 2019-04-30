d9ad33a04d8f11c649ea8d403295fc853d222cd4
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-30 19:05:58 +0200

    Add routes for step 6

--- bookingsystem/routes/web.php
+++ bookingsystem/routes/web.php
@@ -23,10 +23,12 @@ Route::get('/booking-rom', 'BookingController@show2')->name('booking.show-step2'
 Route::get('/booking-fasiliteter', 'BookingController@show3')->name('booking.show-step3');
 Route::get('/booking-registrer', 'BookingController@show4')->name('booking.show-step4');
 Route::get('/booking-sammendrag', 'BookingController@show5')->name('booking.show-step5');
+Route::get('/booking-takk', 'BookingController@show6')->name('booking.show-step6');
 Route::post('/booking-rom', 'BookingController@create2')->name('booking.create-step2');
 Route::post('/booking-fasiliteter', 'BookingController@create3')->name('booking.create-step3');
 Route::post('/booking-registrer', 'BookingController@create4')->name('booking.create-step4');
 Route::post('/booking-sammendrag', 'BookingController@create5')->name('booking.create-step5');
+Route::post('/booking-takk', 'BookingController@create6')->name('booking.create-step6');
 
 
 
