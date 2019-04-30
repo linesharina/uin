e2cf334d3fa695e6ae82cb14e5489db29c2efddd
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-30 19:05:28 +0200

    Add step 6

---
+++ bookingsystem/resources/views/booking/create-step6.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <article class="booking-form booking-form-step-5">
+        <h2>Takk for din bestilling.</h2>
+        <p>Takk for at du valgte Havnehotellet i Halden. Vi ser frem til ditt opphold!</p>
+    </article>
+@endsection
