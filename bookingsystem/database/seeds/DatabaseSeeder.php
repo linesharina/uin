231252acd014e8b91dda73344b124eaaa4de97a9
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-05-01 13:34:36 +0200

    Make seeder generate correct number of rooms

--- bookingsystem/database/seeds/DatabaseSeeder.php
+++ bookingsystem/database/seeds/DatabaseSeeder.php
@@ -19,20 +19,34 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 
         $room_types = [
-            'single' => 990,
-            'double' => 1290,
-            'suite' => 1990,
-            'wedding' => 2490,
+            'single' => [
+                'price' => 990,
+                'count' => 10
+            ],
+            'double' => [
+                'price' => 1290,
+                'count' => 8
+            ],
+            'suite' => [
+                'price' => 1990,
+                'count' => 2
+            ],
+            'wedding' => [
+                'price' => 2490,
+                'count' => 1
+            ]
         ];
 
         $rooms = [];
-        foreach ($room_types as $name => $price) {
-            $room = Room::create([
-                'room_type' => $name,
-                'room_price' => $price,
-            ]);
-
-            array_push($rooms, $room);
+        foreach ($room_types as $name => $roomtype) {
+            for($i = 0; $i < $roomtype['count']; $i++) {
+                $room = Room::create([
+                    'room_type' => $name,
+                    'room_price' => $roomtype['price'],
+                ]);
+    
+                array_push($rooms, $room);
+            }
 
             // $booking = Booking::create([
             //     'people' => 2,
