6ccc28591a83741656287e1d49a5db19ecffa45f
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-05-01 23:50:59 +0200

    Add link to admin site

--- bookingsystem/resources/views/layouts/app.blade.php
+++ bookingsystem/resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('admin.admin-bookings') }}">{{ __('Admin') }}</a>
+                                <a class="nav-link" href="{{ route('admin.admin-bookings') }}">{{ __('Alle bookinger') }}</a>
                             </li>
                             {{-- @if (Route::has('register'))
                                 <li class="nav-item">
@@ -49,9 +49,12 @@
                                 </li>
                             @endif --}}
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.admin-bookings') }}">{{ __('Alle bookinger') }}</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->firstname }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
