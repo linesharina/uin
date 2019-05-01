cda1465712c9039ed35f5a5274d930754e9eb5ee
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-05-01 13:35:11 +0200

    Make step2 remember active rooms

--- bookingsystem/resources/views/booking/create-step2.blade.php
+++ bookingsystem/resources/views/booking/create-step2.blade.php
@@ -9,11 +9,24 @@
 
                 <div class="step-2-sec">
                     @foreach ($room_types as $room_type)
+                        @php
+                            $checked = false;
+
+                            if (session('rooms')) {
+                                foreach (session('rooms') as $room) {
+                                    if ($room === $room_type->name) {
+                                        $checked = true;
+                                        continue;
+                                    }
+                                }
+                            }
+                        @endphp
+
                         <section class="step-2-sec-inner">
                             <img src="{{ asset('images/' . __($room_type->name) . '.jpg') }}" alt="Illutrasjon av {{ __($room_type->name) }}">
                             <span class="step-2-sec-inner-check">
                                 {{-- <input type="checkbox" name="rooms[]" id="form-room-{{ $room_type->name }}" value="{{ $room_type->name }}" {{ session('rooms') === $room_type->name ? 'checked' : null }}> --}}
-                                <input type="checkbox" name="rooms[]" id="form-room-{{ $room_type->name }}" value="{{ $room_type->name }}" @foreach(session('rooms') as $room) @if($room === $room_type->name) {{'checked'}} @endif @endforeach >
+                                <input type="checkbox" name="rooms[]" id="form-room-{{ $room_type->name }}" value="{{ $room_type->name }}" {{ $checked ? 'checked' : null }}>
                                 <label for="form-room-{{ $room_type->name }}">{{ __(ucfirst($room_type->name)) }}</label>
                             </span>
                             <p>NOK {{ $room_type->price }},- per natt</p>        
