534eea5895ef2031a2a64cf223e5204ae5eb20da
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-05-02 10:56:04 +0200

    Fix parking validaton

--- bookingsystem/app/Facility.php
+++ bookingsystem/app/Facility.php
@@ -5,6 +5,7 @@ namespace App;
 use DB;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use App\Facility;
 use App\BookingUser;
 use App\BookingUserFasility;
 
@@ -15,16 +16,23 @@ class Facility extends Model
         // Antall parkeringsplasser
         $parking_spots = 14;
 
+        // Parking facility
+        $parking_facility = Facility::where('name', 'parking')->firstOrFail();
+
         // Finn opptatte parkeringsplasser
         $parkings_unavailable = BookingUserFacility::select(
             'booking_user_facilities.id', 
             'booking_user_facilities.facility_id'
         )->join('booking_users', 'booking_users.id', '=', 'booking_user_facilities.booking_user_id')
         ->join('facilities', 'facilities.id', '=', 'booking_user_facilities.facility_id')
-        ->where('booking_user_facilities.facility_id', 3)
+        ->join('bookings', 'bookings.id', '=', 'booking_users.id')
+        ->where('booking_user_facilities.facility_id', $parking_facility->id)
+        ->where('bookings.check_in', '<', $to)
+        ->where('bookings.check_out', '>', $from)
         ->groupBy('booking_user_facilities.facility_id')
         ->select('booking_user_facilities.facility_id', DB::raw('count(*) as total'))
         ->get();
+
         $parkings_unavailable = $parkings_unavailable->pluck('total')->toArray();
         
         // Regn ut hvor mange parkeringsplasser som er tilgjengelige
